c5ebe3a2d7b51acfdb751e80020c9ef4a670c1bc
Miquéias Coelho <81934809+miqueiaspcoelho@users.noreply.github.com>
2022-11-08 01:40:34 +0000

    Correções Feitas:   *termo
      Atualizações & Features novas:
      *doStatement
      *returnStatement
      *letStatement
      *subrotinecall
      *expression
      *expressionlist
    
      A fazer:
      *terminar statements
      *resolver bug do doStatement quando tem (x,y+1)
      *demais regras, ir seguindo o exemplo do professor.

--- Parser.java
+++ Parser.java
@@ -2,42 +2,61 @@ class Parser {
   private  Scanner scanner;//léxico é entrada do analisador sintático - chamando por demanda
   private Token currentToken;//token atual
   private StringBuilder xmlOutput = new StringBuilder();//arquivo xml
-  
-  public Parser (Scanner scanner){//construtor
+  private  int countOp = 0;
+
+  //construtor
+  public Parser (Scanner scanner){
     this.scanner = scanner;
   }
 
-  private void nextToken() {//pega próximo token
+  //pega próximo token
+  private void nextToken() {
     currentToken = scanner.nextToken();    
   }
 
 
-  void start(){//inicia parser
+  //inicia parser
+  void start(){
     currentToken = scanner.nextToken();
      //System.out.println(currentToken.getText());
     statements(currentToken);
    
   }
+  
 
-  public void statements(Token currentToken){//checa declarações
+  //checa declarações
+  public void statements(Token currentToken){
     switch(currentToken.getText()){
       case "let":
         parserLet();
       break;
+      case "return":
+        parserReturn();
+      break;
+      case "do":
+        parserDo();
+      break;
     }
   }
-
-  void parserLet(){//let
+  
+//letStatement -> 'let' identifier( '[' expression ']' )? '=' expression ';’
+  void parserLet(){
     printNonTerminal("letStatement");
     expectToken(currentToken, "let");
     expectToken(currentToken, "identifier");
+    if(currentToken.getText().equals("[")){
+      expectToken(currentToken,"[");
+      parserExpression();
+      expectToken(currentToken,"]");
+        }
     expectToken(currentToken, "=");
     parserExpression();
     expectToken(currentToken, ";");
     printNonTerminal("/letStatement");
   }
 
-  private void expectToken(Token currentToken, String tokenExpect){//token esperado
+  //token esperado
+  private void expectToken(Token currentToken, String tokenExpect){
     if(currentToken.getText().equals(tokenExpect) || currentToken.getType().equals(tokenExpect)){
       //System.out.println(currentToken.getText());
       xmlOutput.append(String.format("%s\r\n", currentToken.toString()));
@@ -48,91 +67,134 @@ class Parser {
     }
   }
 
-  private void printNonTerminal(String nterminal) {//adiciona ao xml os não terminais
+  //adiciona ao xml os não terminais
+  private void printNonTerminal(String nterminal) {
         xmlOutput.append(String.format("<%s>\r\n", nterminal));
   }
 
-  public String XMLOutput() {//retornando em string o xml
+  //retornando em string o xml
+  public String XMLOutput() {
     return xmlOutput.toString();
   }
 
-  void parserTerm(){//termo
-    printNonTerminal("term");
+  //'do' subrotineCall ';' com alguns probleminhas ainda
+  void parserDo(){
+    printNonTerminal("doStatement");
+    expectToken(currentToken, "do");
+    expectToken(currentToken, "identifier");
+    parserSubrotineCall();
+    expectToken(currentToken, ";");
+    printNonTerminal("/doStatement");
+  }
+
+  //ReturnStatement -> 'return' expression? ';'
+  void parserReturn(){
+    printNonTerminal("return");
+    expectToken(currentToken, "keyword");
+    if(!currentToken.getText().equals(";")){
+      parserExpression();
+    }
+    expectToken(currentToken, ";");
+    printNonTerminal("/return");
+  }
+
+  //expressionList - falta revisar algumas coisas
+  void parserExpressionList(){
+    printNonTerminal("expressionList");
+    if(!currentToken.getText().equals(")")){
+      parserExpression();
+    }
+    while(currentToken.getText().equals(",")){
+      expectToken(currentToken,",");
+      parserExpression();
+    }
+    printNonTerminal("/expressionList");
+  }
+
+  //subrotineCall - falta revisar algumas coisas
+  void parserSubrotineCall(){
+    if(currentToken.getText().equals("(")){
+      expectToken(currentToken,"(");
+      parserExpressionList();
+      expectToken(currentToken,")");
+    }
+    else{
+      expectToken(currentToken,".");
+      expectToken(currentToken,"identifier");
+      expectToken(currentToken,"(");
+      parserExpressionList();
+      expectToken(currentToken,")");
+    }
+  }
+  
+  //termo - as demais partes dele estão sendo implementadas
+  void parserTerm(){
     switch(currentToken.getType()){
       case "integerConstant":
+        printNonTerminal("term");
         expectToken(currentToken, "integerConstant");
+        printNonTerminal("/term");
+        countOp-=1;
       break;
-    }
-    printNonTerminal("/term");
+
+      case "stringConstant":
+        printNonTerminal("term");
+        expectToken(currentToken, "stringConstant");
+        printNonTerminal("/term");
+        countOp-=1;
+      break;
+
+      case "identifier":
+        printNonTerminal("term");
+        expectToken(currentToken, "identifier");
+        printNonTerminal("/term");
+        countOp-=1;
+      break;
+
+      case "keyword":
+        switch(currentToken.getText()){
+          case "true":
+          case "false":
+          case "null":
+          case "this":
+            printNonTerminal("term");
+            expectToken(currentToken, currentToken.getText());
+            printNonTerminal("/term");
+            countOp-=1;
+          break;
+          default:
+            ;
+        }
+        
+    }  
   }
 
-  void parserOperation(){
-    
-    switch (currentToken.getText()){
-      case "+":
-        printNonTerminal("operation");
-        expectToken(currentToken,"+");
-        printNonTerminal("/operation");
-        parserTerm();
-        parserOperation();
-      break;
-      case "-":
-        printNonTerminal("operation");
-        expectToken(currentToken,"-");
-        printNonTerminal("/operation");
-        parserTerm();
-        parserOperation();
-      break;
-      case "/":
-        printNonTerminal("operation");
-        expectToken(currentToken,"/");
-        printNonTerminal("/operation");
-        parserTerm();
-        parserOperation();
-      break;
-      case "*":
-        printNonTerminal("operation");
-        expectToken(currentToken,"*");
-        printNonTerminal("/operation");
-        parserTerm();
-        parserOperation();
-      break;
-      case "&":
-        printNonTerminal("operation");
-        expectToken(currentToken,"&");
-        printNonTerminal("/operation");
-        parserTerm();
-        parserOperation();
-      break;
-      case "<":
-        printNonTerminal("operation");
-        expectToken(currentToken,"<");
-        printNonTerminal("/operation");
-        parserTerm();
-        parserOperation();
-      break;
-      case ">":
-        printNonTerminal("operation");
-        expectToken(currentToken,">");
-        printNonTerminal("/operation");
-        parserTerm();
-        parserOperation();
-      break;
-      case "|":
-        printNonTerminal("operation");
-        expectToken(currentToken,"|");
-        printNonTerminal("/operation");
-        parserTerm();
-        parserOperation();
-      break;
+  //checando se é um operador válido
+  boolean isOperator(Token c){
+    String aux = c.getText();
+    if(c.getType().equals("symbol")){
+      if(aux.equals("+") || aux.equals("-")||aux.equals("*")||aux.equals("/")){
+        countOp+=1;
+        return true;
+      }else if(aux.equals("&")||aux.equals("|")||aux.equals(">")||aux.equals("<")||aux.equals("=")){
+        countOp+=1;
+        return true;
+      }
     }
+    return false;
   }
 
+  //'expression' -> term(op term)*';'
   void parserExpression(){
+   
     printNonTerminal("expression");
     parserTerm();
-    parserOperation();
+    while(isOperator(currentToken)==true && countOp==0){
+        printNonTerminal("operation");
+        expectToken(currentToken,currentToken.getText());
+        printNonTerminal("/operation");
+        parserTerm();
+    }
     printNonTerminal("/expression");
   }
-
 }
